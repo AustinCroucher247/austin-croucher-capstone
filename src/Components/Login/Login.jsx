eeeeaf9dea3dfa8ee6ccba0e25fde93bff1e7539
Austin Croucher <austin.croucher@gmail.com>
2023-03-30 15:25:17 -0400

    added login card

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -3,13 +3,19 @@ import Header from '../Header/Header';
 
 function Login() {
     return (
-        <div>
+        <>
             <Header />
-        </div>
+            <div class="card">
+                <h2>Login</h2>
+                <form action="" method="post">
+                    <input type="text" name="username" placeholder="Username" />
+                    <input type="password" name="password" placeholder="Password" />
+                    <input type="submit" value="Login" />
+                    <div class="error">Incorrect username or password</div>
+                </form>
+            </div>
+        </>
     );
 }
 
 export default Login;
-
-
-
