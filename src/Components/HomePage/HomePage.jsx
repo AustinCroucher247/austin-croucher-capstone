179ef1aa0651e54c92dda16054b2566a4f6dd7b2
Austin Croucher <austin.croucher@gmail.com>
2023-03-30 14:46:31 -0400

    working on header

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,15 +1,43 @@
 import './HomePage.scss'
 import HeaderLogo from '../../assets/RetroGames.jpg'
+import { Link } from 'react-router-dom';
 
 function HomePage() {
     return (
         <div>
-            <header className='header'>
-                <img className='header--logo' src={HeaderLogo} alt="" />
-                <div className='header--logo--right'></div>
-            </header>
+            <div className='header--container'>
+                <header className='header'>
+                    <Link to="./" style={{ textDecoration: 'none' }}>
+                        <img className='header--logo' src={HeaderLogo} alt="" />
+                    </Link>
 
-        </div>
+                    <div className='header--logo--right'>
+                        <nav>
+                            <ul className='list'>
+                                <Link to={'./'} >
+                                    <li className='list--item'>
+                                        Home
+                                    </li>
+                                </Link>
+                                <li className='list--item'>
+                                    Game Room
+                                </li>
+                                <li className='list--item'>
+                                    Active Streams
+                                </li>
+                                <li className='list--item'>
+                                    Leaderboards
+                                </li>
+                                <li className='list--item'>
+                                    About us
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </header>
+            </div>
+
+        </div >
     );
 }
 
