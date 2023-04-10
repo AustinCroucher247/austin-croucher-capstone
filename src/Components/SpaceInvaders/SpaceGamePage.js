1e178f304dd346dafb8b4387e2dd003ad4926bb3
Austin Croucher <austin.croucher@gmail.com>
2023-04-10 11:41:09 -0400

    change back

--- src/Components/SpaceInvaders/SpaceGamePage.js
+++ src/Components/SpaceInvaders/SpaceGamePage.js
@@ -91,13 +91,13 @@ function SpaceGamePage(props) {
         setShowModal(false);
         setRenderGame(true);
         setShowChat(true);
-        setShowControls(false);
-        setShowLeaderboard(true);
+        setShowControls(false)
+        setShowLeaderboard(true)
 
-        const username = localStorage.getItem('username');
-        socketRef.current.emit('createRoom', username, (createdRoomId) => {
+        socketRef.current.emit('createRoom', (createdRoomId) => {
             socketRef.current.emit('joinRoom', createdRoomId);
         });
+
     };
 
     const [rooms, setRooms] = useState({});
